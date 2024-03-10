a3659284f49134dc2cdcc8614629c075ba1c7eb4
Kean Bayneslow <kean.bayneslow@gmail.com>
2024-03-11 10:26:49 +1100

    adds initial file structure and form outline

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { Routes, Route, Navigate } from "react-router-dom"
+import { NewNote } from "./NewNote"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container className="my-4">
+  <Routes>
+    <Route path="/" element={<h1>Home</h1>} />
+    <Route path="/new" element={<NewNote />} />
+    <Route path="/:id">
+      <Route index element={<h1>Show</h1>} />
+      <Route path="edit" element={<h1>Edit</h1>} />
+    </Route>
+    <Route path="*" element={<Navigate to="/" />} />
+  </Routes>
+  </Container>
   )
 }
 
---
+++ src/NoteForm.tsx
@@ -0,0 +1,33 @@
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import CreatableReactSelect from "react-select/creatable"
+
+export function NoteForm() {
+  return (
+    <Form>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect isMulti />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="markdown">
+              <Form.Label>Body</Form.Label>
+              <Form.Control required as="textarea" rows={15} />
+        </Form.Group>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+            <Button type="submit" variant="outline-primary">Save</Button>
+            <Button type="button" variant="outline-secondary">Cancel</Button>
+        </Stack>
+      </Stack>
+    </Form>
+  );
+}
